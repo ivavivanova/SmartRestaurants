dd3c996c5313be908bce39c3afc0e026c037aac2
ivavivanova <ivavivanova@outlook.com>
2019-01-23 16:24:34 +0200

    Update EF code first

---
+++ Infrastructure/Answer.cs
@@ -0,0 +1,28 @@
+namespace Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Answer")]
+    public partial class Answer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Answer()
+        {
+            FeedbackQuestionAnswers = new HashSet<FeedbackQuestionAnswer>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<FeedbackQuestionAnswer> FeedbackQuestionAnswers { get; set; }
+    }
+}
