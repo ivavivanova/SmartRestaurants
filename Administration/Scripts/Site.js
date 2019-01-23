4609c817c172cb04d997bd1ac1b2dbda9126a432
ivavivanova <ivavivanova@outlook.com>
2019-01-24 01:36:34 +0200

    Add dataTables to administration module

---
+++ Administration/Scripts/Site.js
@@ -0,0 +1,24 @@
+﻿$.extend($.fn.dataTable.defaults, {
+    "pageLength": 10,
+    "lengthChange": false,
+    "language": {
+        "emptyTable": "Няма резервации",
+        "info": "От _START_ до _END_ от общо _TOTAL_ резервации",
+        "infoEmpty": "Показване на 0 от 0 резервации",
+        "infoFiltered": "(Филтрирани от _MAX_ общо резервации)",
+        "loadingRecords": "Зареждане...",
+        "processing": "Обработка...",
+        "search": "Търсене:",
+        "zeroRecords": "Няма намерени резервации",
+        "paginate": {
+            "previous": "<  ",
+            "next": "  >",
+            "first": "|<<  ",
+            "last": "  >>|"
+        }
+    }
+});
+
+$(document).ready(function () {
+    $('.dataTable').DataTable();
+});
