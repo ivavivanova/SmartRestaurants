f8fdba9bb77fd54f976c45bfa3626600b1195638
ivavivanova <ivavivanova@outlook.com>
2019-01-26 18:01:38 +0200

    Add restaurant view

--- TableSelectionModule/App_Start/BundleConfig.cs
+++ TableSelectionModule/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace TableSelectionModule
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/Site").Include(
+                        "~/Scripts/Site.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
