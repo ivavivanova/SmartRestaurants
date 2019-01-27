594ada4951415e22542b5b27a88f5ab1fdea4e04
ivavivanova <ivavivanova@outlook.com>
2019-01-27 22:38:33 +0200

    Show path to the table when click on it

--- TableSelectionModule/Scripts/Site.js
+++ TableSelectionModule/Scripts/Site.js
@@ -1,14 +1,21 @@
 ﻿$(document).on('click', '.color-green', function () {
     var $btn = $(this),
-        url = $('.restaurant').data('url');
+        url = $('.restaurant').data('url'),
+        tableNumber = $btn[0].id;
 
-    $.ajax({
-        url: url,
-        type: 'Post',
-        data: { tableNum: $btn[0].id },
-        success: function () {
-            window.location.reload();
-        }
-    });
+    $btn.removeClass("color-green").addClass("color-yellow");
+    $('.baseLine').removeClass('hidden');
+    $('path.' + tableNumber).removeClass('hidden');
+
+    setTimeout(function () {
+        $.ajax({
+            url: url,
+            type: 'Post',
+            data: { tableNum: tableNumber },
+            success: function () {
+                window.location.reload();
+            }
+        });
+    }, 5000);
 });
 
